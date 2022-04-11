3bd537232a8dcc9d7049fccbd7637d343de74ff0
Holden Davis <59069546+holden-davis-uca@users.noreply.github.com>
2022-04-11 16:15:30 -0500

    Minor Login unit test updates

--- Battleship/ClientCommunication/LoginDataTest.java
+++ Battleship/ClientCommunication/LoginDataTest.java
@@ -13,9 +13,23 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class LoginDataTest {
+	String[] users = {"username1", "username2", "username3"};
+	String[] passwords = {"password1", "password2", "password3"};
 
 	@Test
-	void test() {
+	void testgetUsername() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testsetUsername() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testgetPassword() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testsetPassword() {
 		fail("Not yet implemented");
 	}
 
--- Battleship/ClientGUI/LoginPanel.java
+++ Battleship/ClientGUI/LoginPanel.java
@@ -37,9 +37,37 @@ public class LoginPanel extends JPanel {
 	{
 //		LoginControl controller = new LoginControl(container, client);
 //		client.setLoginControl(controller);
-		JPanel grid = new JPanel(new GridLayout(3,2,0,10));
-//		grid.add(*);
-		this.add(grid);
+		
+	    JPanel labelPanel = new JPanel(new GridLayout(2, 1, 5, 5));
+	    errorLabel = new JLabel("", JLabel.CENTER);
+	    errorLabel.setForeground(Color.RED);
+	    JLabel instructionLabel = new JLabel("Enter username and password", JLabel.CENTER);
+	    labelPanel.add(errorLabel);
+	    labelPanel.add(instructionLabel);
+
+	    JPanel loginPanel = new JPanel(new GridLayout(2, 2, 5, 5));
+	    JLabel usernameLabel = new JLabel("Username:", JLabel.RIGHT);
+	    usernameField = new JTextField(10);
+	    JLabel passwordLabel = new JLabel("Password:", JLabel.RIGHT);
+	    passwordField = new JPasswordField(10);
+	    loginPanel.add(usernameLabel);
+	    loginPanel.add(usernameField);
+	    loginPanel.add(passwordLabel);
+	    loginPanel.add(passwordField);
+	    
+	    JPanel buttonPanel = new JPanel();
+	    JButton submitButton = new JButton("Submit");
+	    submitButton.addActionListener(lc);
+	    JButton cancelButton = new JButton("Cancel");
+	    cancelButton.addActionListener(lc);    
+	    buttonPanel.add(submitButton);
+	    buttonPanel.add(cancelButton);
+
+	    JPanel grid = new JPanel(new GridLayout(3, 1, 0, 10));
+	    grid.add(labelPanel);
+	    grid.add(loginPanel);
+	    grid.add(buttonPanel);
+	    this.add(grid);
 		
 	}
 
