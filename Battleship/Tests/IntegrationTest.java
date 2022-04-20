2bb4f3f62653491a89bf53ebd3ea16d20ac95c4f
Holden Davis <59069546+holden-davis-uca@users.noreply.github.com>
2022-04-20 14:31:55 -0500

    Added Database, LoginControl, LoginData, and LoginPanel to class diagram
    Additionally created skeleton integration test

---
+++ Battleship/Tests/IntegrationTest.java
@@ -0,0 +1,22 @@
+//Holden Davis - Team #6
+//CSCI 4490 - CRN 30660 - Spring 2022
+/*
+ * IntegrationTest.java
+ * 
+ * Provides JUnit 5 Integration Testing for the entire project
+ */
+
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntegrationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
