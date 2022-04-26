065b5aa92d0717cc6059b59e41ed431c0a2000bb
Riley <jwilliams107@cub.uca.edu>
2022-04-25 23:17:30 -0500

    Udated GameControl and GameData
    Added some of Kevin's parts as his computer wasn't connecting

--- Battleship/ClientCommunication/GameControl.java
+++ Battleship/ClientCommunication/GameControl.java
@@ -1,15 +1,75 @@
 package ClientCommunication;
 
+import java.awt.event.ActionEvent;
+import java.util.Scanner;
+
 import javax.swing.*;
 import ClientGUI.*;
 
 public class GameControl {
-	private GamePanel container;
+	private JPanel container;
 	private GameClient client;
 
 	public GameControl(JPanel container, GameClient client) {
-		this.container = (GamePanel) container;
+		this.container = container;
 		this.client = client;
+	}
+	public void actionPerformed(ActionEvent ae) {
+		//gametsss
+		int gameover = 0;
+	    int error = 0;
+	    int x = 0; 
+	    int y =0;
+	    int horizontal = 0;
+		int[][][] gameBoard = new int[5][5][2];
+		for (int i = 0; i < 3; i++)
+		{
+
+		    if (horizontal == 1)
+		    {				
+		    		for(int j = 0; j < (i + 2); j++) {
+		    			gameBoard[x + j][y][0] = 1;
+		    			gameBoard[x + j][y][1] = i+2;
+		    		}
+		    }
+		    else
+		    {
+		    	for(int j = 0; j < (i + 2); j++) {
+		    		gameBoard[x][y + j][0] = 1;
+		    		gameBoard[x][y + j][1] = i+2;
+		    	}	
+		    }
+		}
+		int ship1 = 0;
+		int ship2 = 0;
+		int ship3 = 0;
+
+			    if(gameBoard[x][y][0] == 1) 
+			    {
+			    	if(gameBoard[x][y][1] == 2) {
+			    		ship1 = ship1 + 1;
+				    	System.out.println("Ship Hit!");
+			    	}
+			    	if(gameBoard[x][y][1] == 3) {
+			    		ship2 = ship2 + 1;
+				    	System.out.println("Ship Hit!");
+			    	}
+			    	if(gameBoard[x][y][1] == 4) {
+			    		ship3 = ship3 + 1;
+				    	System.out.println("Ship Hit!");
+			    	}
+			    }
+			    else
+			    {
+			    	System.out.println("Miss!");
+			    }
+			    if(ship1 == 2 && ship2 == 3 && ship3 == 4) 
+			    {
+			    	gameover = 1;
+			    	System.out.println("GameOver!");
+
+			    }
+			
 
 	}
 }
--- Battleship/ClientCommunication/GameData.java
+++ Battleship/ClientCommunication/GameData.java
@@ -1,30 +1,45 @@
 package ClientCommunication;
 
 import java.util.*;
-
+//ssss
 public class GameData {
-	private ArrayList<Integer> xcoords;
-	private ArrayList<Integer> ycoord;
-
-	public GameData(ArrayList<Integer> xcoords, ArrayList<Integer> ycoords) {
-		this.xcoords = xcoords;
-		this.ycoord = ycoords;
+	int[][][] gameboard = new int[5][5][2];
+	int ship1 = 0;
+	int ship2 = 0;
+	int ship3 = 0;
+	public GameData(int[][][] gameboard, int ship1, int ship2, int ship3) {
+		this.gameboard = gameboard;
 	}
-
-	public ArrayList<Integer> getXcoords() {
-		return xcoords;
+	
+	
+	public int[][][] getGamboard(int[][][] gameboard) {
+		return gameboard;
 	}
-
-	public void setXcoords(ArrayList<Integer> xcoords) {
-		this.xcoords = xcoords;
+	public int getShip1(int ship1) {
+		
+		return ship1;
 	}
-
-	public ArrayList<Integer> getYcoord() {
-		return ycoord;
+	public int getShip2(int ship2) {
+		
+		return ship2;
 	}
-
-	public void setYcoord(ArrayList<Integer> ycoord) {
-		this.ycoord = ycoord;
+	
+	public int getShip3(int ship3) {
+		
+		return ship3;
 	}
-
+	public void setGamboard(int[][][] gameboard) {
+		this.gameboard = gameboard;
+	}
+	public void setShip1(int ship1) {
+		
+		this.ship1 = ship1;
+	}
+	public void setShip2(int ship2) {
+		this.ship2 = ship2;
+	}
+	
+	public void setShip3(int ship3) {
+		this.ship3 = ship3;
+	}
 }
